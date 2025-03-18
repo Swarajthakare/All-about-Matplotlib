5c74eb04021226655796e1a97019150c3ae5265f
SWARAJ THAKARE <137782161+Swarajthakare@users.noreply.github.com>
2025-03-18 23:45:29 +0530

    Add files via upload

---
+++ 8 plt.savefig().py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+plt.pie([40,30,20])
+plt.savefig("pie_char",dip =10, quality = 99)
+plt.show()
+
+
+# In[ ]:
+
+
+
+
