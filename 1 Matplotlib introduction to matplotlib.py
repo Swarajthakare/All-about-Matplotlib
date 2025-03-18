4b8e28bfa97ef37e7ae94f658bc0d838ec684d4b
SWARAJ THAKARE <137782161+Swarajthakare@users.noreply.github.com>
2025-03-18 23:40:55 +0530

    Add files via upload

---
+++ 1 Matplotlib introduction to matplotlib.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Agenda
+# 
+# - What is Matplotlib?
+# - History of Matplotlib?
+# - Matplotlib Graphs?
+# - Dependency of Matplotlib?
+# - Installation of Matplitlib
+# - Importing Matplotlib:
+# - Study Materials
+
+# #### What is matplotlib?
+# - Matplotlib is 2D and 3D plotting python library.
+# - It produce high quality graph. 
+# - It also support animation.
+# 
+# 
+# 
+# #### History of Matplotlib
+# - American neurobiologist
+# - John D.Hunter(1968 - 2012)
+# - Author og Matplotlib
+# - Written in Python 
+# - Initial released in 2003
+# 
+# #### Matplotlib Graphs 
+# - Histogram
+# - Scatterplots
+# - Pie plot
+# - Error Charts
+# - Power Spectra
+# 
+# 
+# #### Dependency
+# - Numpy
+# - Pandas
+# - 
+# 
+# #### Installation of Matplotlib
+# 
+# 
+# 
+# 
+# #### Importing Matplotlib
+# - from matplotlib import pyplot as plt
+# - import matplotlib.pyplot as plt
+# 
+# 
+
+# In[ ]:
+
+
+
+
